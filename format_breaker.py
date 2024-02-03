e271c40ce61c207364245a7f57574f7a9e34b815
John Ranson <john@phaselead.com>
2024-02-03 17:17:39 -0500

    Added a variety of little ended integers.

--- format_breaker.py
+++ format_breaker.py
@@ -355,7 +355,7 @@ class Remnant(DataType):
 
 
 class Int32sl(Bytes):
-    """Reads 4 bytes as a unsigned, little endian integer"""
+    """Reads 4 bytes as a signed, little endian integer"""
 
     def __init__(self, name=None, address=None, copy_source=None) -> None:
         super().__init__(4, name, address, copy_source)
@@ -364,14 +364,54 @@ class Int32sl(Bytes):
         return int.from_bytes(super()._decode(data), "little", signed=True)
 
 
+class Int32ul(Bytes):
+    """Reads 4 bytes as a unsigned, little endian integer"""
+
+    def __init__(self, name=None, address=None, copy_source=None) -> None:
+        super().__init__(4, name, address, copy_source)
+
+    def _decode(self, data):
+        return int.from_bytes(super()._decode(data), "little", signed=False)
+
+
 class Int16sl(Bytes):
-    """Reads 2 bytes as a unsigned, little endian integer"""
+    """Reads 2 bytes as a signed, little endian integer"""
 
     def __init__(self, name=None, address=None, copy_source=None) -> None:
         super().__init__(2, name, address, copy_source)
 
     def _decode(self, data):
         return int.from_bytes(super()._decode(data), "little", signed=True)
+
+
+class Int16ul(Bytes):
+    """Reads 2 bytes as a unsigned, little endian integer"""
+
+    def __init__(self, name=None, address=None, copy_source=None) -> None:
+        super().__init__(2, name, address, copy_source)
+
+    def _decode(self, data):
+        return int.from_bytes(super()._decode(data), "little", signed=False)
+
+
+class Int8sl(Byte):
+    """Reads 1 bytes as a unsigned, little endian integer"""
+
+    def __init__(self, name=None, address=None, copy_source=None) -> None:
+        super().__init__(name, address, copy_source)
+
+    def _decode(self, data):
+        return int.from_bytes(super()._decode(data), "little", signed=True)
+
+
+class Int8ul(Byte):
+    """Reads 1 bytes as a unsigned, little endian integer"""
+
+    def __init__(self, name=None, address=None, copy_source=None) -> None:
+        super().__init__(name, address, copy_source)
+
+    def _decode(self, data):
+        return int.from_bytes(super()._decode(data), "little", signed=False)
 
 
 class Float32l(Bytes):
@@ -392,5 +432,3 @@ class Float64l(Bytes):
 
     def _decode(self, data):
         return struct.unpack("<d", super()._decode(data))[0]
-
-
